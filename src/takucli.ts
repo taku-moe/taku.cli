6a2fb89142798a5c6ea1db949be063b2622fba28
azur1s <73991665+azur1s@users.noreply.github.com>
2021-09-22 23:07:56 +0700

    geowo best
    Co-Authored-By: George Tsotsos <34042825+Geoxor@users.noreply.github.com>

--- src/takucli.ts
+++ src/takucli.ts
@@ -1,28 +1,38 @@
 import Input from "./taku/input";
+import chalk from "chalk";
 // @ts-ignore
 import * as fetch from "node-fetch";
 
 class TakuCLI {
   public username: string | undefined;
   public password: string | undefined;
-  private _authToken: string | undefined;
+  private authToken: string | undefined;
 
   public async run() {
     await Input.getInput();
+    this.username = Input.username;
+    this.password = Input.password;
+
     this.login();
   }
 
   public async login() {
-    const body = {username: Input.username, password: Input.password};
+    const body = { username: this.username, password: this.password };
 
-    const response = await fetch("https://backend.taku.moe/v1/login", {
-      method: 'POST',
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-    console.log(await response.json());
+    try {
+      const response = await fetch("https://backend.taku.moe/v1/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      const { token } = await response.json();
+      this.authToken = token;
+      console.log(chalk.hex("#F90069")(`Welcome`), chalk.hex("#FAFAFA")(this.username), chalk.hex("#F90069")(`to taku.cli!`));
+    } catch (error) {
+      console.log(error);
+    }
   }
 }
 
