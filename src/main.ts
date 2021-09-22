0a5e3af0e1d74fe8e121ff8cf968060fd84354bd
N1kO23 <niko.huuskonen.00@gmail.com>
2021-09-22 23:37:11 +0300

    refactored IO

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,97 @@
-console.clear();
-
+import Taku from "taku.js";
 import chalk from "chalk";
+// @ts-ignore
+import * as fetch from "node-fetch";
+import { InputOutputHandler } from "./taku/input";
 import { version } from "../package.json";
-import TakuCLI from "./takucli";
 
-console.log(chalk.hex("#F90069")(`Welcome to taku.cli! ${version}`));
+class TakuCLI {
+  // User infos
+  public username: string | undefined;
+  public password: string | undefined;
+  private authToken: string | undefined;
+  private keepAlive: boolean = true;
+  private ioHandler: InputOutputHandler = new InputOutputHandler();
 
-TakuCLI.run();
+  // CLI Stuff
+  public messageCount: number = 50;
+  public app: any;
+  private messages: any;
+  private messageInput = [
+    {
+      type: "text",
+      name: "input",
+      message: "Input:",
+    },
+  ];
+
+  public async run() {
+    this.ioHandler.println(`Welcome to taku.cli! ${version}`);
+    this.ioHandler.println(`The terminal size is ${this.ioHandler.getTerminal().width}, ${this.ioHandler.getTerminal().height}`);
+    this.ioHandler.print("Username: ");
+    this.username = await this.ioHandler.getInput(true);
+    this.ioHandler.print("Password: ");
+    this.password = await this.ioHandler.getInput(true, true);
+
+    await this.login();
+    // this.getMessage();
+    while (!this.keepAlive) {
+      await this.sendMessage();
+    }
+    this.ioHandler.println("Logging off...");
+    process.exit();
+  }
+
+  public async login() {
+    this.ioHandler.println("Logging in...");
+    const body = { username: this.username, password: this.password };
+
+    try {
+      const response = await fetch("https://backend.taku.moe/v1/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(body),
+      });
+      const { token } = await response.json();
+      this.authToken = token;
+      this.app = new Taku(this.authToken, false, "");
+      this.ioHandler.println("Welcome " + this.username + " to taku.cli!");
+    } catch (error) {
+      this.ioHandler.println("Error while logging in! " + error);
+    }
+  }
+
+  // for now, we are getting from @global
+  public async getMessage() {
+    this.ioHandler.println("Getting @global messages...");
+    try {
+      const response = await fetch(`https://backend.taku.moe/v1/message/@global/0/${this.messageCount}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: this.authToken,
+        },
+      });
+
+      this.messages = await response.json();
+    } catch (error) {
+      this.ioHandler.println("Error while getting messages!" + error);
+    }
+  }
+
+  public async sendMessage() {
+    // const { input } = await prompts(this.messageInput);
+    // if (input == "!!quit") {
+    //   this.keepAlive = false;
+    // }
+    // this.term(`${this.username}: `, input);
+    // this.app.send(input);
+  }
+
+  public handleInput(input: string) {}
+}
+
+const taku = new TakuCLI();
+taku.run();
--- src/taku/input.ts
+++ src/taku/input.ts
@@ -1,28 +1,64 @@
-const prompts = require("prompts");
+const keypress = require("keypress");
+import { createTerminal, Terminal } from "terminal-kit";
+export class InputOutputHandler {
+  private hideOutput: boolean = false;
+  private term: Terminal = createTerminal();
+  private inputBuffer: string = "";
 
-class Input {
-  public username!: string;
-  public password!: string;
-  private loginQuestion = [
-    {
-      type: "text",
-      name: "username",
-      message: "Username:",
-      validate: (username: string) => (!username ? "Please enter username!" : true),
-    },
-    {
-      type: "password",
-      name: "password",
-      message: "Password:",
-      alidate: (username: string) => (!username ? "Please enter password!" : true),
-    },
-  ];
+  constructor() {}
+  private handleInput(ch: string, key: {}, hideOutput: boolean) {
+    if (hideOutput) this.term("\b*");
+    else this.term(ch);
+  }
 
-  public async getInput() {
-    const login = await prompts(this.loginQuestion);
-    this.username = login.username;
-    this.password = login.password;
+  /**
+   *
+   * @param newLine Set to true if you want newline to be added after input
+   * @param hideOutput Set to true if you want to hide the output with *
+   * @returns the input string
+   */
+  public async getInput(newLine: boolean = false, hideOutput: boolean = false): Promise<string> {
+    return new Promise((resolve) => {
+      let inputBuffer = "";
+      keypress(process.stdin);
+      process.stdin.on("keypress", (ch, key) => {
+        if (key?.name === "return") {
+          if (newLine) this.print("\n");
+          resolve(inputBuffer);
+        }
+        if (key && key.ctrl && key.name == "c") {
+          process.stdin.pause();
+        }
+        this.handleInput(ch, key, hideOutput);
+        inputBuffer += key;
+      });
+      process.stdin.setRawMode(true);
+      process.stdin.resume();
+    });
+  }
+
+  private async sleep(msec: number) {
+    return new Promise((resolve) => setTimeout(resolve, msec));
+  }
+
+  public getHideOutput(): boolean {
+    return this.hideOutput;
+  }
+  public setHideOutput(value: boolean) {
+    this.hideOutput = value;
+  }
+  public getTerminal(): Terminal {
+    return this.term;
+  }
+  public setTerminal(value: Terminal) {
+    this.term = value;
+  }
+
+  // TODO: Add color support
+  public print(value: string) {
+    this.term(value);
+  }
+  public println(value: string) {
+    this.term(value + "\n");
   }
 }
-
-export default new Input();
